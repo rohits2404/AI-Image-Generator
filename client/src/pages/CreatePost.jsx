4b3a213a193589d3703db23128d6d5c274d1b574
Rohit Sharma <rohits8731@gmail.com>
2025-08-15 13:10:49 +0530

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -26,7 +26,7 @@ const CreatePost = () => {
         if (form.prompt) {
             try {
                 setGeneratingImg(true);
-                const response = await fetch("http://localhost:8080/api/v1/dalle", {
+                const response = await fetch(`${import.meta.env.VITE_BASE_API_URL}/api/v1/dalle`, {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
                     body: JSON.stringify({ prompt: form.prompt }),
@@ -55,7 +55,7 @@ const CreatePost = () => {
         setLoading(true);
 
         try {
-            const response = await fetch("http://localhost:8080/api/v1/post", {
+            const response = await fetch(`${import.meta.env.VITE_BASE_API_URL}/api/v1/post`, {
             method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(form),
@@ -169,5 +169,6 @@ const CreatePost = () => {
         </section>
     );
 };
+
 
 export default CreatePost;
