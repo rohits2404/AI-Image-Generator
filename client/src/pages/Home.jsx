75e014dc413e2e6d8557e153e687f4669b94319e
Rohit Sharma <rohits8731@gmail.com>
2025-08-15 13:09:11 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -39,7 +39,7 @@ const Home = () => {
     const fetchPosts = async () => {
         setLoading(true);
         try {
-            const response = await fetch('http://localhost:8080/api/v1/post', {
+            const response = await fetch(`${import.meta.env.VITE_BASE_API_URL}/api/v1/post`, {
                 method: 'GET',
                 headers: { 'Content-Type': 'application/json' },
             });
@@ -143,5 +143,6 @@ const Home = () => {
         </section>
     );
 };
+
 
 export default Home;
